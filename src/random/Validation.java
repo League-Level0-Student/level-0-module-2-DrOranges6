1c45c30311208bf67e3968e9a3662f8f69219784
DrOranges6 <league@WTS-iMac-11.attlocal.net>
2019-04-23 18:28:53 -0700

    :crying_cat_face:

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -17,6 +17,19 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
+		if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "You Suck!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "You were never loved");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "You Failure!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "Your brothers the favorite child!");
+		} else if (randomNumber== 5) {
+			JOptionPane.showMessageDialog(null, "You Hufflepuff!");
+		} else { if (randomNumber == 6)
+			JOptionPane.showMessageDialog(null, "Get a life you idiot!");
+		}
 
 		// 2. Repeat all the code above 10 times
 		
